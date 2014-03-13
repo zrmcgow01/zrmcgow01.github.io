962548e93274f8318506d116b45cda47286169a8
zrmcgow01 <zachary.mcgowan@tufts.edu>
2014-03-12 20:53:10 -0400

    print each prediction

--- transit/my_mbta.js
+++ transit/my_mbta.js
@@ -146,8 +146,8 @@ function initialize(position) {
 
 function parse_API_data(scheduleData){
 	for(var w in scheduleData["schedule"]){
-		for(var p in scheduleData["schedule"][w]){
-			console.log(scheduleData["schedule"][w]["Destination"]);
+		for(var p in scheduleData["schedule"][w]["Predictions"]){
+			console.log(scheduleData["schedule"][w]["Predictions"][p]);
 		}
 	}
 	return scheduleData["line"];
