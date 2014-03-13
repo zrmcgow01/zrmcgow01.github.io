fa52d5a27f9e9be378682e92dc395292fe8034d2
zrmcgow01 <zachary.mcgowan@tufts.edu>
2014-03-12 20:10:51 -0400

    print json object schedule to log

--- transit/my_mbta.js
+++ transit/my_mbta.js
@@ -10,7 +10,7 @@ function parse_json(){
 		if(xhr.readyState==4 && xhr.status==200){
 			scheduleData = JSON.parse(xhr.responseText);
 			line_color = scheduleData["line"];
-			console.log(scheduleData);
+			console.log(scheduleData["schedule"]);
 			if(navigator.geolocation){
 				navigator.geolocation.getCurrentPosition(initialize);
 			}
